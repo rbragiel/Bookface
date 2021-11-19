a16a1e3de1c9abb53f4deb34d2d61d90f27c00f1
Jakub Sukiennik <kuba1207710@gmail.com>
2021-11-19 17:16:02 +0100

    adding testing setup (#1)

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,12 +1,17 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
   extends: [
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:jest-dom/recommended",
+    "plugin:testing-library/react",
+    "plugin:jest/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
