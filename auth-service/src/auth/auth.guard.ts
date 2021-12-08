e013b4b0e77ae6f6056b23bcea5664cfcd336411
Jakub Sukiennik <kuba1207710@gmail.com>
2021-12-08 21:11:01 +0100

    open api

--- auth-service/src/auth/auth.guard.ts
+++ auth-service/src/auth/auth.guard.ts
@@ -11,9 +11,16 @@ export class AuthGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = <AppRequest>context.switchToHttp().getRequest();
-    const token = request.headers.authorization;
 
-    if (!token) {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader) {
+      return false;
+    }
+
+    const [bearer, token] = authHeader.split(' ');
+
+    if (!token || bearer !== 'Bearer') {
       return false;
     }
 
--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -1,6 +1,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,12 +11,22 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
-
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 5000;
 
   app.setGlobalPrefix('api');
+
+  const config = new DocumentBuilder()
+    .setTitle('AuthService')
+    .setDescription('Auth service for Bookface app')
+    .setVersion('1.0')
+    .addTag('auth')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(port, () => {
     logger.log(`App running on port ${port}`);
