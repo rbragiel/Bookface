0b885b44df91b079225d1424b2dbe3a5e3a26fc0
Jakub Sukiennik <kuba1207710@gmail.com>
2021-12-10 18:52:26 +0100

    not found page

--- frontend/src/components/layout/index.tsx
+++ frontend/src/components/layout/index.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <Flex minH="100vh" h="100%" flexDir="column" w="100%">
+    <Flex h="100%" flexDir="column" w="100%">
       <Outlet />
     </Flex>
   );
