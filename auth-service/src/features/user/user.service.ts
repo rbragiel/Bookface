5242260a5d07ca840ac821fa65ed57d2016d698c
Jakub Sukiennik <kuba1207710@gmail.com>
2022-01-06 00:24:39 +0100

    prepare frontend api logic

--- auth-service/src/features/user/user.service.ts
+++ auth-service/src/features/user/user.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { FindOptions } from 'sequelize';
-import { UserRegisterDto } from './user.dto';
+import { FindOptions, Op } from 'sequelize';
+import {
+  UserRegisterDto,
+  UserSearchDto,
+  UsersSearchResultDto,
+} from './user.dto';
 import { User, UserRole } from './user.model';
 
 @Injectable()
 export class UserService {
   constructor(@InjectModel(User) private readonly userModel: typeof User) {}
+
+  private searchUsersLimit = 15;
+
+  private excludeOptions = [
+    'password',
+    'updatedAt',
+    'role',
+    'description',
+    'isActivated',
+    'birthday',
+    'joined',
+  ];
 
   async create(user: UserRegisterDto) {
     const newUser = await this.userModel.create({
@@ -26,5 +42,39 @@ export class UserService {
       where: { email },
     });
     return user;
+  }
+
+  async findByNickname(nickname: string) {
+    const user = await this.userModel.findOne({
+      where: { nickname },
+    });
+    return user;
+  }
+
+  async searchUser(query: string, skip: number): Promise<UsersSearchResultDto> {
+    const likeQuery = `${query}%`;
+    const users = await this.userModel.findAll({
+      where: {
+        [Op.or]: [
+          {
+            nickname: {
+              [Op.like]: likeQuery,
+            },
+          },
+          {
+            email: {
+              [Op.like]: likeQuery,
+            },
+          },
+        ],
+      },
+      attributes: {
+        exclude: this.excludeOptions,
+      },
+      limit: this.searchUsersLimit,
+      offset: skip * this.searchUsersLimit,
+    });
+
+    return { users: users as UserSearchDto[] };
   }
 }
---
+++ frontend/src/api/invitations.ts
@@ -0,0 +1,9 @@
+class InvitationApiEndpoints {
+  static baseUrl = "http://localhost:8080/api/invitations";
+  static invitedUrl = "/all/invited";
+  static inviteesUrl = "/all/invitees";
+  static inviteUrl = "/invite";
+  static acceptUrl = "/accept";
+  static rejectUrl = "/reject";
+}
+export { InvitationApiEndpoints };
